8ecd982c9c6d929fde794665cd1794a93fd7180b
Lautaro Pedroza <Lautimotoe99@gmail.com>
2023-04-21 20:57:59 -0300

    creando al app de contador en React

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <div className='App'>
 
       <div className='freecodecamp-logo-contenedor'>
-       
+       <img className='freeCodeCamp-logo' src={require('./imagenes/logo_large.png')} />
       </div>
 
       <div className='contenedor-principal'>
